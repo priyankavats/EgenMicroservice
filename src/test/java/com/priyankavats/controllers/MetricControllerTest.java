a04061f8401d994de04942cdc84054a56ea1e9b6
Priyanka Vats <pvats@asu.edu>
2016-08-12 05:09:26 -0700

    Added unit tests for controllers and rules

--- src/test/java/com/priyankavats/controllers/MetricControllerTest.java
+++ src/test/java/com/priyankavats/controllers/MetricControllerTest.java
@@ -4,9 +4,16 @@ import static org.hamcrest.Matchers.equalTo;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -14,21 +21,67 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-import com.priyankavats.controllers.MetricController;
+import com.priyankavats.dao.MetricDao;
+import com.priyankavats.models.Metric;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest
 public class MetricControllerTest {
 
+	@InjectMocks
+	private MetricController controller = new MetricController();
+
+	@Mock
+	MetricDao metricDaoMock;
+
 	private MockMvc mvc;
 
 	@Before
 	public void setUp() throws Exception {
-		mvc = MockMvcBuilders.standaloneSetup(new MetricController()).build();
+		MockitoAnnotations.initMocks(this);
+		mvc = MockMvcBuilders.standaloneSetup(controller).build();
+	}
+
+	@SuppressWarnings("unchecked")
+	@Test
+	public void getEmptyListWhenMetricDaoThrowsException() throws Exception {
+		Mockito.when(metricDaoMock.readAll()).thenThrow(NullPointerException.class);
+		mvc.perform(MockMvcRequestBuilders.get("/metrics/read").accept(MediaType.APPLICATION_JSON))
+				.andExpect(status().isOk()).andExpect(content().string(equalTo("[]")));
 	}
 
 	@Test
-	public void getHello() throws Exception {
-		mvc.perform(MockMvcRequestBuilders.post("/metrics/create").accept(MediaType.APPLICATION_JSON));
+	public void getValidListWhenMetricDaoReturnsMetrics() throws Exception {
+		Metric m = new Metric();
+		m.setTimeStamp(131331331);
+		m.setValue(150);
+		List<Metric> mList = new ArrayList<Metric>();
+		mList.add(m);
+
+		Mockito.when(metricDaoMock.readAll()).thenReturn(mList);
+		mvc.perform(MockMvcRequestBuilders.get("/metrics/read").accept(MediaType.APPLICATION_JSON))
+				.andExpect(status().isOk())
+				.andExpect(content().string(equalTo("[{\"timeStamp\":131331331,\"value\":150}]")));
+	}
+
+	@Test
+	public void getValidListOfTimeRangeWhenMetricDaoReturnsMetrics() throws Exception {
+		Metric m = new Metric();
+		m.setTimeStamp(1471002713317l);
+		m.setValue(150);
+
+		Metric m2 = new Metric();
+		m2.setTimeStamp(1471002713327l);
+		m2.setValue(150);
+
+		List<Metric> mList = new ArrayList<Metric>();
+		mList.add(m);
+		mList.add(m2);
+
+		Mockito.when(metricDaoMock.readByTimeRange(1471002713316l, 1471003519705l)).thenReturn(mList);
+		mvc.perform(MockMvcRequestBuilders.get("/metrics/readByTimeRange/1471002713316/1471003519705")
+				.accept(MediaType.APPLICATION_JSON)).andExpect(status().isOk())
+				.andExpect(content().string(equalTo(
+						"[{\"timeStamp\":1471002713317,\"value\":150},{\"timeStamp\":1471002713327,\"value\":150}]")));
 	}
 }
